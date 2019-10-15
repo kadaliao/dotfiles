0ee3683b35b586e238400b77c706a44f1a49b1d4
Kada Liao <kadaliao@gmail.com>
2019-10-15 08:09:19 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git wget htop tmux pipenv python3 vim neovim coreutils psutils ranger fish nodejs npm yarn powerline
+
+
+# nerdfont
+brew tap homebrew/cask-fonts
+brew cask install font-hack-nerd-font
+
+
+# pip
+pip3 install autopep8 flake8 pylint jedi neovim
+
+# lastpass
+sudo cpan install Capture::Tiny
+brew install lastpass-cli
