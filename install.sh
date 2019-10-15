f040bd83f4ef2ea9915f0c3e9810e5ee3999ca2e
Kada Liao <kadaliao@gmail.com>
2019-10-15 18:29:47 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,15 @@
 # homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install git wget htop tmux pipenv python3 vim neovim coreutils psutils ranger fish nodejs npm yarn powerline
+brew install git wget htop tmux pipenv python2 python3 vim neovim coreutils psutils ranger fish nodejs npm yarn powerline fzf
 
 
 # nerdfont
 brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font
 
+
+pip2 install powerline-status
 
 # pip
 pip3 install autopep8 flake8 pylint jedi neovim
