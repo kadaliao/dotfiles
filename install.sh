9036981019bfa259672ffcf3bdba82e7f56b6689
Kada Liao <kadaliao@gmail.com>
2019-10-16 17:15:38 +0800

    add ctags to install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install git wget htop tmux pipenv python2 python3 vim neovim coreutils psutils ranger fish nodejs npm yarn powerline fzf rg
+brew install git wget htop tmux pipenv python2 python3 vim neovim coreutils psutils ranger fish nodejs npm yarn powerline fzf rg ctags
 
 
 # nerdfont
