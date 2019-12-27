a1e3efe0f8735cd0ba6ebf4af61f668e06d635c6
Kada Liao <kadaliao@gmail.com>
2019-12-27 12:37:46 +0800

    add tmuxp and rename profile to localhost

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -42,14 +42,13 @@ set -g fish_user_paths "$HOME/.gem/ruby/2.6.0/bin" $fish_user_paths
 set -g fish_user_paths "$HOME/go/bin" $fish_user_paths
 
 # /usr/loca/bin and ~/bin
-# set -g fish_user_paths "/usr/local/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 set -g fish_user_paths "$HOME/bin" $fish_user_paths
 
 # rg configuration
 set -gx RIPGREP_CONFIG_PATH "$HOME/.ripgreprc"
-set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
 
 # n configuration
-set -gx N_PREFIX "$HOME/.n"
-set -gx fish_user_paths "$N_PREFIX/bin" $fish_user_paths
-set -gx N_NODE_MIRROR "https://npm.taobao.org/mirrors/node"
+# set -gx N_PREFIX "$HOME/.n"
+# set -gx fish_user_paths "$N_PREFIX/bin" $fish_user_paths
+# set -gx N_NODE_MIRROR "https://npm.taobao.org/mirrors/node"
