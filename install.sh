8da3908d889d8bbeb7db9effc2383c5ad059f12c
Kada Liao <kadaliao@gmail.com>
2020-03-05 14:15:18 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
 # homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install git wget htop tmux pipenv python2 python3 vim neovim coreutils psutils ranger fish nodejs npm yarn powerline fzf rg ctags autojump
+brew install git wget htop tmux pipenv python3 vim neovim coreutils psutils ranger fish nodejs npm yarn fzf rg ctags autojump
 
 # nerdfont
 brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font
 
 # pip
-pip3 install autopep8 flake8 pylint jedi neovim docopt
+pip3 install powerline-status autopep8 flake8 pylint jedi neovim docopt
 
 # lastpass
 sudo cpan install Capture::Tiny
