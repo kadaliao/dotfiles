23b540086e8fd9adbd15ff6a207cd28c1c009245
Kada Liao <kadaliao@gmail.com>
2020-05-25 15:57:13 +0800

    for linux

--- install.sh
+++ install.sh
@@ -1,25 +1,36 @@
 #!/usr/bin/env sh
 
 # homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+if [ $(uname) == 'Darwin' ];then
+	echo "Install for macOS"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+	# nerdfont
+	brew tap homebrew/cask-fonts
+	brew cask install font-hack-nerd-font
+
+	# lastpass
+	sudo cpan install Capture::Tiny
+	brew install lastpass-cli
+
+else
+	echo "Install for linux..."
+	git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+	mkdir ~/.linuxbrew/bin
+	ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
+	eval $(~/.linuxbrew/bin/brew shellenv)
+	echo 'eval $(~/.linuxbrew/bin/brew shellenv)' >> ~/.bashrc
+fi
 
 brew install git wget htop tmux pipenv python3 go vim neovim coreutils psutils ranger fish nodejs npm yarn fzf rg ctags autojump fx fpp libmagic
 
-# nerdfont
-brew tap homebrew/cask-fonts
-brew cask install font-hack-nerd-font
 
 # lazygit
 brew install jesseduffield/lazygit/lazygit
 brew install lazygit
-
-pip install neovim
 
 python3 -m pip install powerline-status autopep8 flake8 pylint jedi neovim docopt tmuxp
 
 # pistol
 env GO111MODULE=on go get -u github.com/doronbehar/pistol/cmd/pistol
 
-# lastpass
-sudo cpan install Capture::Tiny
-brew install lastpass-cli
