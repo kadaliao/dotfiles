4c5ca33bd69ca114d5ea2c06e857de43cb50c64f
Kada Liao <kadaliao@gmail.com>
2020-06-26 18:46:03 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -13,24 +13,61 @@ if [ $(uname) == 'Darwin' ];then
 	sudo cpan install Capture::Tiny
 	brew install lastpass-cli
 
+	# autojump is only for macOS
+	brew install -v autojump
+
 else
 	echo "Install for linux..."
 	git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
-	mkdir ~/.linuxbrew/bin
-	ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
+	mkdir -p ~/.linuxbrew/bin
+	ln -sf ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
 	eval $(~/.linuxbrew/bin/brew shellenv)
 	echo 'eval $(~/.linuxbrew/bin/brew shellenv)' >> ~/.bashrc
 fi
 
-brew install git wget htop tmux pipenv python3 go vim neovim coreutils psutils ranger fish nodejs npm yarn fzf rg ctags autojump fx fpp libmagic glibc tig
+# brew install files
+brew install -v git
+brew install -v wget
+brew install -v htop
+brew install -v tmux
+brew install -v pipenv
+brew install -v python3
+brew install -v go
+brew install -v vim
+brew install -v neovim
+brew install -v coreutils
+brew install -v psutils
+brew install -v ranger
+brew install -v fish
+brew install -v nodejs
+brew install -v npm
+brew install -v yarn
+brew install -v fzf
+brew install -v rg
+brew install -v ctags
+brew install -v fx
+brew install -v fpp
+brew install -v libmagic
+brew install -v glibc
+brew install -v tig
 
 
 # lazygit
 brew install jesseduffield/lazygit/lazygit
 brew install lazygit
 
-python3 -m pip install powerline-status autopep8 flake8 pylint jedi neovim docopt tmuxp ruamel.yaml jinja2
+python3 -m pip install virtualenv
+python3 -m pip install powerline-status
+python3 -m pip install autopep8
+python3 -m pip install flake8
+python3 -m pip install pylint
+python3 -m pip install jedi
+python3 -m pip install neovim
+python3 -m pip install docopt
+python3 -m pip install tmuxp
+python3 -m pip install ruamel.yaml
+python3 -m pip install jinja2
+
 
 # pistol
 env GO111MODULE=on go get -u github.com/doronbehar/pistol/cmd/pistol
-
