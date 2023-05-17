59948c928d4cd4c7f015055a9480796cd4eaddfe
Kada Liao <kadaliao@gmail.com>
2023-05-17 23:12:28 +0800

    update tmux-start

--- dotfiles/local/bin/tmux-start.sh
+++ dotfiles/local/bin/tmux-start.sh
@@ -1,52 +1,44 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-if [ -f ~/.bashrc ]; then
-  . ~/.bashrc
-fi
+# Source .zshrc if exists
+[[ -f ~/.zshrc ]] && source ~/.zshrc
 
 ############################### FOLDER ICONS ###################################
 
-# LOGFILE="/Users/liaoxingyi/Desktop/log"
-# touch $LOGFILE
-# exec 1>$LOGFILE
-# exec 2>&1
+# Disable logging for now
+#exec 1>/dev/null
+#exec 2>&1
 
 folder="$(pwd)"
-
-# Construct the path for the folder icon
 FOLDER_ICON_PATH="$folder/.icon"
 
-if [ -f $FOLDER_ICON_PATH ]; then
-    folder_icon="$(cat $FOLDER_ICON_PATH)"
+# Read folder icon from .icon file, or use default icon
+if [[ -f "$FOLDER_ICON_PATH" ]]; then
+    folder_icon="$(cat "$FOLDER_ICON_PATH")"
 else
-    echo "Could not find an .icon file in $folder, using the default icon 📁"
+    echo "Could not find an .icon file in '$folder', using the default icon 📁"
     folder_icon="📁"
 fi
 
-# Strip the path and leave the folder name
-folder_name="$(basename $folder)"
-
-# Construct the session name
+# Get the folder name and construct the session name
+folder_name="$(basename "$folder")"
 SESSION="$folder_icon  $folder_name"
 
 # Attach to Tmux
-
-if [ -z "$TMUX" ]; then
+if [[ -z "$TMUX" ]]; then
     # We're not inside Tmux
     echo "Attach to $SESSION"
     tmux attach-session -d -t "$SESSION" || tmux new-session -s "$SESSION"
 else
     # We're inside Tmux
-
-    # if [ -z "$(tmux list-sessions | grep $folder_name -s)" ]; then
-    if ! tmux has-session -t "$SESSION" 2>/dev/null; then
-        # Create a new dettached session
+    if ! tmux has-session -t "$SESSION" &>/dev/null; then
+        # Create a new detached session
         echo "Create $SESSION"
-        tmux new -ds "$SESSION" 
+        tmux new -ds "$SESSION"
     fi
 
     # Switch to the session
-    if [[ $(tmux switch-client -t "$SESSION") ]] ; then
+    if tmux switch-client -t "$SESSION"; then
         echo "Switch client to $SESSION"
     else
         echo "Switch client to $SESSION failed"
